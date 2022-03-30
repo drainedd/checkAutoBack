d1c5f04c429cd5c5a88d1199a6b529f080b73381
drainedd <flax.dx@gmail.com>
2022-03-30 20:41:55 +0300

    chatBot

--- client/src/pages/MainPage/MainPage.js
+++ client/src/pages/MainPage/MainPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import chatImg from './chatbot.png'
+
 
 import './MainPage.css'
 
@@ -7,11 +7,7 @@ const MainPage = () => {
     
     return (
         <div className="section">
-            <div className='chatBot'>
-                <a href='https://t.me/CheckAuto116_Bot' target="blank">
-                    <img src={chatImg} alt="" width='45px' height='45px'/>
-                </a>
-            </div>
+           
                 <h1>CheckAuto</h1>
                 <h4>Мы подберем для вас оптимальное предложение на рынке <br></br>с гарантией технической и юридической чистоты</h4>
                 
